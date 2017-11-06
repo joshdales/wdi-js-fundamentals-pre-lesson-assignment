40ddbdb37a766bfe63c29061c19c2f5e6646bc92
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 15:44:26 -0500

    Set variables for all ghosts

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -12,3 +12,6 @@ var inky = {};
 inky.colour = 'Cyan';
 inky.personality = 'Shadow';
 console.log(inky);
+
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
