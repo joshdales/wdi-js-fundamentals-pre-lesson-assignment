e6fc9d522b224833f246101b61f6daf449517002
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 15:46:03 -0500

    Add comments to pacman objects

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -8,10 +8,12 @@ console.log(ghosts[1]);
 console.log(ghosts[2]);
 console.log(ghosts[3]);
 
+// pacman-objects-1
 var inky = {};
 inky.colour = 'Cyan';
 inky.personality = 'Shadow';
 console.log(inky);
 
+// pacman-objects-2
 var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
 console.log(ghosts);
